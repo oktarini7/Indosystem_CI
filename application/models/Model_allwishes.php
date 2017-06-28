5e4a5bcd37c33b287b3e380fbea7a720d091e8b7
Oktarini Wandan Sari <oktarini7@gmail.com>
2017-06-29 03:47:53 +0700

    fix minor typo

--- application/models/Model_allwishes.php
+++ application/models/Model_allwishes.php
@@ -18,8 +18,8 @@ class Model_allwishes extends CI_Model{
 		foreach ($query->result_array() as $row){
 			$wishes.= '<div class="wishes" id="wish_'.$row['id'].'">';
 			$wishes.= '<div id="name_'.$row['id'].'">Name: '.$row['name'].'</div>';
-			$wishes.= '<div id="address_'.$row['id'].'">Name: '.$row['address'].'</div>';
-			$wishes.= '<div id="phone_'.$row['id'].'">Name: '.$row['phone'].'</div>';
+			$wishes.= '<div id="address_'.$row['id'].'">Address: '.$row['address'].'</div>';
+			$wishes.= '<div id="phone_'.$row['id'].'">Phone: '.$row['phone'].'</div>';
 			$wishes.= '<div id="note_'.$row['id'].'">Note: '.$row['note'].'</div>';
 			$wishes.= '<div id="db_'.$row['id'].'"><a href="#" onclick="return false;" onmousedown="deleteNote(\''.$row['id'].'\',\'wish_'.$row['id'].'\');" title="DELETE NOTE">delete note</a></div>';
 			$wishes.= '</div>';
